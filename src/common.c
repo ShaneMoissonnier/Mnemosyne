0846c2474c61fd07820a7f6f2318b26efc8fe687
Shane Moissonnier <shane.moissonnier@gmail.com>
2021-10-02 17:27:30 +0200

    Moved get_align macro and fix typo in macro

--- src/common.c
+++ src/common.c
@@ -7,18 +7,6 @@
 #define MEMORY_SIZE 4096
 #endif
 
-// La valeur d'alignement de nos données (8 octets pour un système Linux)
-#define ALIGN 8;
-// On définit notre valeur de saut par les ALIGN-1 bits de poids faibles
-#define LSB_STEP (ALIGN - 1);
-
-/*
- * On crée un masque pour supprimer les ALIGN-1 bits de poids faibles et on l'applique à notre taille
- * (Note : ici notre saut (LSB_STEP) est ajouté à notre taille avant application du masque
- * pour obtenir un "alignement minimum" correct si 0 < size < ALIGN)
-*/
-#define get_align(size) ((size + LSB_STEP) & ~(LSB_STEP));
-
 static char memory[MEMORY_SIZE];
 
 void *get_memory_adr() { return memory; }
