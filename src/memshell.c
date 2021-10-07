f659456c089acb4090a497863d6e18c4842ebc3a
Shane Moissonnier <shane.moissonnier@gmail.com>
2021-10-07 23:13:15 +0200

    Added fancy memory display in memshell.c

--- src/memshell.c
+++ src/memshell.c
@@ -7,7 +7,8 @@
 #define TAILLE_BUFFER 128
 #define MAX_ALLOCATIONS 128
 
-void aide() {
+void aide()
+{
     fprintf(stderr, "Aide :\n");
     fprintf(stderr, "Saisir l'une des commandes suivantes\n");
     fprintf(stderr, "\n");
@@ -24,27 +25,56 @@ void aide() {
     fprintf(stderr, "M         :   afficher la liste de tous les emplacements "
                     "memoire (libres et occupes)\n");
     fprintf(stderr, "m         :   afficher le dump de la memoire\n");
+    fprintf(stderr, "s         :   affiche le tableau de tous les emplacements "
+                    "memoire (en-tete, libre et occupes)\n");
     fprintf(stderr, "h         :   afficher cette aide\n");
     fprintf(stderr, "q         :   quitter ce programme\n");
     fprintf(stderr, "\n");
 }
 
-void afficher_zone(void *adresse, size_t taille, int free) {
+void afficher_zone(void *adresse, size_t taille, int free)
+{
     printf("Zone %s, Adresse : %lu, Taille : %lu\n", free ? "libre" : "occupee",
            (unsigned long)adresse, (unsigned long)taille);
 }
 
-void afficher_zone_libre(void *adresse, size_t taille, int free) {
+void afficher_tas(void *adresse, size_t taille, int free)
+{
+    switch (free)
+    {
+    case 0:
+        printf("%11d |%11s |%11ld |%11d |%11ld\n", (int)(adresse - get_memory_adr()), "BUSY", taille, (int)((adresse + sizeof(fb)) - get_memory_adr()), taille - sizeof(fb));
+        break;
+    case 1:
+        printf("%11d |%11s |%11ld |\n", (int)(adresse - get_memory_adr()), "FREE", taille);
+        break;
+    case 2:
+        printf("%11d |%11s |%11ld |\n", (int)(adresse - get_memory_adr()), "HEADER", taille);
+        break;
+    }
+}
+
+void afficher_entete()
+{
+    printf("%11s |%11s |%11s |%11s |%11s\n", "@", "TYPE", "SIZE", "user@", "user size");
+    printf("---------------------------------------------------------------\n");
+    afficher_tas(get_memory_adr(), sizeof(header), 2);
+}
+
+void afficher_zone_libre(void *adresse, size_t taille, int free)
+{
     if (free)
         afficher_zone(adresse, taille, 1);
 }
 
-void afficher_zone_occupee(void *adresse, size_t taille, int free) {
+void afficher_zone_occupee(void *adresse, size_t taille, int free)
+{
     if (!free)
         afficher_zone(adresse, taille, 0);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     char buffer[TAILLE_BUFFER];
     char commande;
     char *adresse;
@@ -57,11 +87,13 @@ int main(int argc, char **argv) {
     aide();
     mem_init();
 
-    while (1) {
+    while (1)
+    {
         fprintf(stderr, "? ");
         fflush(stdout);
         commande = getchar();
-        switch (commande) {
+        switch (commande)
+        {
         case 'a':
             scanf("%d", &taille);
             ptr = mem_alloc(taille);
@@ -91,6 +123,10 @@ int main(int argc, char **argv) {
             break;
         case 'M':
             mem_show(afficher_zone);
+            break;
+        case 's':
+            afficher_entete();
+            mem_show(afficher_tas);
             break;
         case 'm':
             printf("[ ");
